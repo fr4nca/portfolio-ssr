4ecf31f0e1c168a7a7ac572dfb2b9001ce6ae6e8
Victor Almeida França <fr4ncacc@gmail.com>
2019-05-31 15:00:20 -0300

    deploy

--- components/Projects/Projects.js
+++ components/Projects/Projects.js
@@ -1,50 +1,57 @@
-import React, { Component } from "react";
-import "./Projects.css";
+import React, { Component } from 'react'
+import './Projects.css'
 
-import ProjectItem from "./ProjectItem";
-import gameduc from "../../static/img/gameduc.png";
-import prototipo from "../../static/img/prototipo.png";
-import naoseesqueca from "../../static/img/naoseesqueca.png";
-import elasporelas from "../../static/img/elasporelas.png";
+import ProjectItem from './ProjectItem'
+import gameduc from '../../static/img/gameduc.png'
+import prototipo from '../../static/img/prototipo.png'
+import naoseesqueca from '../../static/img/naoseesqueca.png'
+import elasporelas from '../../static/img/elasporelas.png'
+import postsocial from '../../static/img/postsocial.png'
 
 const projetos = [
   {
-    nome: "Gameduc",
+    nome: 'Gameduc',
     foto: gameduc,
-    github: "https://github.com/fr4nca/gameduc",
-    live: "http://gameeduc.com.br"
+    github: 'https://github.com/fr4nca/gameduc',
+    live: 'http://gameeduc.com.br'
   },
   {
-    nome: "Protótipo Gameduc",
+    nome: 'Protótipo Gameduc',
     foto: prototipo,
-    github: "https://github.com/fr4nca/prototipoGameficacaoCEUB",
-    live: "https://ecstatic-liskov-084d66.netlify.com/"
+    github: 'https://github.com/fr4nca/prototipoGameficacaoCEUB',
+    live: 'https://ecstatic-liskov-084d66.netlify.com/'
   },
   {
-    nome: "Elas por elas",
+    nome: 'Elas por elas',
     foto: elasporelas,
-    github: "https://github.com/fr4nca/ElasPorElas",
-    live: "https://elasporelas.herokuapp.com/"
+    github: 'https://github.com/fr4nca/ElasPorElas',
+    live: 'https://elasporelas.herokuapp.com/'
   },
   {
-    nome: "Não se esqueça",
+    nome: 'Não se esqueça',
     foto: naoseesqueca,
-    github: "https://github.com/fr4nca/projetoWeb",
-    live: "https://naoseesqueca.herokuapp.com/"
+    github: 'https://github.com/fr4nca/projetoWeb',
+    live: 'https://naoseesqueca.herokuapp.com/'
+  },
+  {
+    nome: 'Post Social',
+    foto: postsocial,
+    github: 'https://github.com/fr4nca/post-social-frontend',
+    live: 'https://sharp-torvalds-825267.netlify.com/'
   }
-];
+]
 
 export default class Projects extends Component {
   render() {
     return (
-      <div id="projects" className="bg-darker p-2">
-        <h2 className="section-heading">Projects</h2>
-        <div className="project-grid">
+      <div id='projects' className='bg-darker p-2'>
+        <h2 className='section-heading'>Projects</h2>
+        <div className='project-grid'>
           {projetos.map(projeto => (
             <ProjectItem key={projeto.nome} project={projeto} />
           ))}
         </div>
       </div>
-    );
+    )
   }
 }
--- components/Skills/Skills.js
+++ components/Skills/Skills.js
@@ -1,99 +1,95 @@
-import React, { Component } from "react";
-import "./Skills.css";
+import React, { Component } from 'react'
+import './Skills.css'
 
 export default class Skills extends Component {
   render() {
     return (
-      <div id="myskills" className="bg-light p-2">
-        <div className="section">
-          <h2 className="section-heading">Skills</h2>
+      <div id='myskills' className='bg-light p-2'>
+        <div className='section'>
+          <h2 className='section-heading'>Skills</h2>
           <hr />
-          <ul id="skills">
-            <ul id="frotend-skills">
+          <ul id='skills'>
+            <ul id='frotend-skills'>
               <h2>Front-end</h2>
-              <li>React/Angular front-end development</li>
-              <li>Handlebars</li>
+              <li>React/Vue/Angular front-end development</li>
               <li>Vanilla HTML5, CSS3 and JavaScript</li>
+              <li>CSS frameworks like Bootstrap, Bulma and Materialize</li>
               <li>Server-side and client-side rendering</li>
-              <li>Redux and Context API</li>
+              <li>Redux, Vuex and Context API</li>
             </ul>
-            <ul id="backend-skills">
+            <ul id='backend-skills'>
               <h2>Back-end</h2>
-              <li>Express </li>
+              <li>Express</li>
+              <li>WSGI</li>
               <li>Nginx</li>
               <li>Websockets</li>
               <li>RESTfull APIs</li>
               <li>GraphQL APIs</li>
             </ul>
-            <ul id="databases-skills">
+            <ul id='databases-skills'>
               <h2>Databases</h2>
               <li>MySQL</li>
               <li>Postgres</li>
               <li>MongoDB</li>
-              <li>Firebase</li>
             </ul>
-            <ul id="security-skills">
+            <ul id='security-skills'>
               <h2>Security</h2>
               <li>JWT authentication</li>
               <li>OAuth</li>
               <li>Role based authorization</li>
             </ul>
-            <ul id="deployment-skills">
-              <h2>Deployment</h2>
+            <ul id='nonspecific-skills'>
+              <h2>Non specific</h2>
+              <li>Cloud computing</li>
               <li>Docker containers</li>
               <li>Container orchestration with Kubernetes</li>
               <li>Docker-compose</li>
-            </ul>
-            <ul id="nonspecific-skills">
-              <h2>Non specific</h2>
-              <li>Cloud computing</li>
               <li>Microservices</li>
               <li>Linux environment</li>
               <li>Server management</li>
-              <li>MVC pattern</li>
             </ul>
           </ul>
-          <ul className="skills-list">
-            <div id="frameworks">
+          <ul className='skills-list'>
+            <div id='frameworks'>
               <li>
-                <i className="fab fa-node-js fa-2x" />
+                <i className='fab fa-node-js fa-2x' />
                 NodeJS
               </li>
               <li>
-                <i className="fab fa-react fa-2x" />
+                <i className='fab fa-react fa-2x' />
                 React
               </li>
               <li>
-                <i className="fas fa-code fa-2x" />
+                <i className='fas fa-code fa-2x' />
                 Back-end
               </li>
             </div>
-            <div id="technologies">
+            <div id='technologies'>
               <li>
-                <i className="fab fa-html5 fa-2x" />
+                <i className='fab fa-html5 fa-2x' />
                 HTML5
               </li>
               <li>
-                <i className="fab fa-css3 fa-2x" />
+                <i className='fab fa-css3 fa-2x' />
                 CSS3
               </li>
               <li>
-                <i className="fab fa-js-square fa-2x" />
+                <i className='fab fa-js-square fa-2x' />
                 JavaScript
               </li>
             </div>
           </ul>
-          <div className="resume">
+          <div className='resume'>
             <a
-              className="btn btn-dark download-resume"
-              target="_blank"
-              href="../../static/pdf/CURRICULO_Victor.pdf"
+              className='btn btn-dark download-resume'
+              target='_blank'
+              href='../../static/pdf/CURRICULO_Victor.pdf'
             >
               Currículo / Resumè
             </a>
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
