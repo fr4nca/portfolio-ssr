5524c7f90d7b5e5565cdec4ed9abbf7c341fc38f
Victor Almeida França <fr4ncacc@gmail.com>
2019-07-25 17:49:55 -0300

    updated curriculum

--- components/Skills/Skills.js
+++ components/Skills/Skills.js
@@ -1,28 +1,30 @@
-import React, { Component } from "react";
-import "./Skills.css";
+import React, { Component } from 'react';
+import './Skills.css';
 
 export default class Skills extends Component {
   render() {
     return (
-      <div id="myskills" className="bg-light p-2">
-        <div className="section">
-          <h2 className="section-heading">Skills</h2>
+      <div id='myskills' className='bg-light p-2'>
+        <div className='section'>
+          <h2 className='section-heading'>Skills</h2>
           <hr />
-          <ul id="skills">
-            <ul id="frotend-skills">
+          <ul id='skills'>
+            <ul id='frotend-skills'>
               <h2>Front-end</h2>
               <li>React</li>
               <li>VueJS</li>
+              <li>Redux, Vuex and Context API state management</li>
+              <li>SASS and SCSS</li>
               <li>Vanilla HTML5, CSS3 and JavaScript</li>
               <li>CSS frameworks like Bootstrap, Bulma and Materialize</li>
               <li>Server-side and client-side rendering</li>
-              <li>Redux, Vuex and Context API state management</li>
             </ul>
-            <ul id="mobile-skills">
+            <ul id='mobile-skills'>
               <h2>Mobile Development</h2>
               <li>React Native</li>
+              <li>Flutter</li>
             </ul>
-            <ul id="backend-skills">
+            <ul id='backend-skills'>
               <h2>Back-end</h2>
               <li>Express</li>
               <li>Django</li>
@@ -32,19 +34,19 @@ export default class Skills extends Component {
               <li>RESTfull APIs</li>
               <li>GraphQL APIs</li>
             </ul>
-            <ul id="databases-skills">
+            <ul id='databases-skills'>
               <h2>Databases</h2>
               <li>Postgres</li>
               <li>MySQL</li>
               <li>MongoDB</li>
             </ul>
-            <ul id="security-skills">
+            <ul id='security-skills'>
               <h2>Security</h2>
               <li>JWT authentication</li>
               <li>OAuth</li>
               <li>Role based authorization</li>
             </ul>
-            <ul id="nonspecific-skills">
+            <ul id='nonspecific-skills'>
               <h2>Non specific</h2>
               <li>Cloud Computing</li>
               <li>Docker</li>
@@ -55,39 +57,39 @@ export default class Skills extends Component {
               <li>Server management</li>
             </ul>
           </ul>
-          <ul className="skills-list">
-            <div id="frameworks">
+          <ul className='skills-list'>
+            <div id='frameworks'>
               <li>
-                <i className="fab fa-node-js fa-2x" />
+                <i className='fab fa-node-js fa-2x' />
                 NodeJS
               </li>
               <li>
-                <i className="fab fa-react fa-2x" />
+                <i className='fab fa-react fa-2x' />
                 React
               </li>
               <li>
-                <i className="fab fa-python fa-2x" />
+                <i className='fab fa-python fa-2x' />
                 Python
               </li>
               <li>
-                <i className="fab fa-html5 fa-2x" />
+                <i className='fab fa-html5 fa-2x' />
                 HTML5
               </li>
               <li>
-                <i className="fab fa-css3 fa-2x" />
+                <i className='fab fa-css3 fa-2x' />
                 CSS3
               </li>
               <li>
-                <i className="fab fa-js-square fa-2x" />
+                <i className='fab fa-js-square fa-2x' />
                 JavaScript
               </li>
             </div>
           </ul>
-          <div className="resume">
+          <div className='resume'>
             <a
-              className="btn btn-dark download-resume"
-              target="_blank"
-              href="../../static/pdf/CURRICULO_Victor.pdf"
+              className='btn btn-dark download-resume'
+              target='_blank'
+              href='../../static/pdf/CURRICULO_Victor.pdf'
             >
               Currículo / Resumè
             </a>
